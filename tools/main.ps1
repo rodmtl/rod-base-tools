e42403d4ad8987d8ca77060521d66c1693426a98
Rod Riveros <rodmtl@outlook.es>
2024-07-03 14:35:33 -0400

    chore: Update tool installation script to use Invoke-Expression instead of iex

--- tools/main.ps1
+++ tools/main.ps1
@@ -1,5 +1,5 @@
 # install chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 # call the scripts to install the tools
 # check if chocolatey is installed
 if (Get-Command choco -ErrorAction SilentlyContinue) {
